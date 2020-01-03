0e27123710861e1a57390c60fa2bdc283c7fe36b
Thomas Barratt <thomas.barratt@selenefinance.com>
2020-01-03 11:48:16 -0500

    add the use of PScredential for authentication

--- source/functions/public/Get-SEPMAccessToken.ps1
+++ source/functions/public/Get-SEPMAccessToken.ps1
@@ -10,10 +10,17 @@ Function Get-SEPMAccessToken {
         The name of the SEPM server.
     .PARAMETER Port
         The port for the SEPM API. Defaults to 8446.
+    .PARAMETER Credential
+        A PSCredential object used for SEPM Authentication. If this parameter is not used, the user will be asked for a
+        username and password.
     .EXAMPLE
-        PS C:\> Get-SEPMAccessToken
+        PS C:\> Get-SEPMAccessToken -ComputerName 'server1'
 
         This example runs the function which will then ask for a username and password.
+    .EXAMPLE
+        PS C:\> Get-SEPMAccessToken -ComputerName 'server1' -Credential $Cred
+
+        This example runs the function that will use a PSCredential object called Cred to authenticate to the SEPM server.
     .INPUTS
         None
     .OUTPUTS
@@ -26,20 +33,29 @@ Function Get-SEPMAccessToken {
         [Parameter(Mandatory=$True,Position=0)]
         [string]$ComputerName,
 
-        $Port = 8446
+        $Port = 8446,
+
+        [Parameter(Mandatory=$False)]
+        [PSCredential]$Credential
     )
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
-    $UserName = Read-Host -Prompt 'UserName'
-    $Password = Read-Host -Prompt 'Password' -AsSecureString
+    If ($Credential) {
+        $UserName = $Credential.UserName
+        $Password = $Credential.GetNetworkCredential().Password
+    } Else {
+        $UserName = Read-Host -Prompt 'UserName'
+        $Password = Read-Host -Prompt 'Password' -AsSecureString
+        $Password = "$([System.Runtime.InteropServices.marshal]::PtrToStringAuto([System.Runtime.InteropServices.marshal]::SecureStringToBSTR($Password)))"
+    }
     $Creds = @{
         username = "$UserName"
-        password = "$([System.Runtime.InteropServices.marshal]::PtrToStringAuto([System.Runtime.InteropServices.marshal]::SecureStringToBSTR($Password)))"
+        password = "$Password"
         domain = ""
     }
     $Body = $Creds | ConvertTo-Json
     $URL = "https://{0}:{1}/sepm/api/v1/identity/authenticate" -f $ComputerName,$Port
     $Response = Invoke-RestMethod -Uri $URL -Method Post -Body $Body -ContentType 'application/json'
-    Remove-Variable Body,Creds,UserName,Password
+    Remove-Variable Body,Creds,UserName,Password,Credential
     $Props = [ordered]@{
         'UserName' = $($Response.username)
         'FullName' = $($Response.fullname)
